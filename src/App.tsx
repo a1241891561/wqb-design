dd4b7376067c867a5856c731d807fe411dc0b931
weiqinbin <wei.qinbing@iwhalecloud.com>
2022-08-03 19:56:07 +0800

    添加button组件.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState, useRef } from "react";
+import Button, { ButtonSize, ButtonType } from "./components/Button";
+// import { Select } from "antd";
+// import "antd/dist/antd.css";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button>111</Button>;
 }
 
 export default App;
